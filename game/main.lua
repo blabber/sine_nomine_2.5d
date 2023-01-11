ea85dad9fccee796745a167b03ca2678995d32c1
Tobias Rehbein <tobias.rehbein@web.de>
2023-01-11 21:51:45 +0100

    Make window resizable

--- game/main.lua
+++ game/main.lua
@@ -4,6 +4,16 @@ sn.level = require 'sn.level'
 local level
 
 function love.load()
+	love.window.setTitle('Sine Nomine 2.5D')
+	love.window.setMode(
+		800,
+		600,
+		{
+			minwidth = 800,
+			minheight = 600,
+			resizable = true
+		})
+
 	local f = love.graphics.setNewFont('font/PressStart2P-vaV7.ttf', 40)
 	level = sn.level.fromString(f, [[
 #####  #####  #####
@@ -53,6 +63,12 @@ function love.update(deltaTime)
 end
 
 function love.keypressed(key)
+	if key == 'f' then
+		love.window.setFullscreen(
+			not love.window.getFullscreen())
+		return
+	end
+
 	level:keypressed(key)
 end
 
